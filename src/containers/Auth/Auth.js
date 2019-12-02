40840c8288ba72bb200009ac65b2cc0fd0cbc3aa
Nickdem <45073853+Nickdem@users.noreply.github.com>
2019-12-02 15:30:27 +0300

    компонент текстового поля

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from './Auth.module.css'
 import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
 
@@ -23,8 +24,14 @@ export default class Auth extends Component {
           <h1>Авторизация</h1>
 
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            <input />
-            <input />
+            <Input
+              label="Email"
+            />
+
+            <Input
+              label="Password"
+              errorMessage={'Error'}
+            />
 
             <Button
               type="success"
