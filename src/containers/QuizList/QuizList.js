cd8d534bd7ca683f3edfad0abf72abe669ab74b0
Nickdem <45073853+Nickdem@users.noreply.github.com>
2019-12-05 23:58:47 +0300

    Компонент процесса загрузки

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import classes from './QuizList.module.css'
 import {NavLink} from 'react-router-dom'
+import Loader from '../../components/UI/Loader/Loader'
 import axios from 'axios'
 
 export default class QuizList extends Component {
 
   state = {
-    quizes: []
+    quizes: [],
+    loading: true
   }
 
   renderQuizes() {
@@ -37,7 +39,8 @@ export default class QuizList extends Component {
       })
 
       this.setState({
-        quizes: quizes
+        quizes: quizes,
+        loading: false
       })
     } catch (e) {
       console.log(e)
@@ -51,9 +54,13 @@ export default class QuizList extends Component {
         <div>
           <h1>Список тестов</h1>
 
-          <ul>
-            {this.renderQuizes()}
-          </ul>
+          { this.state.loading
+            ? <Loader />
+            : <ul>
+                {this.renderQuizes()}
+              </ul>
+          }
+
         </div>
       </div>
     )
